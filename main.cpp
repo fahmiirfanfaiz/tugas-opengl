9698d03c593987820ebd4c9f13566c9a16c87408
Fahmi Irfan Faiz <fahmiirfanfaiz01@gmail.com>
2025-05-14 13:54:55 +0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <tiny_obj_loader.h>
+
+
+int main(){
+	return 0;
+}
